eabfefca7b030b37c7975da338c8497c25d6b2a0
Jérôme Pourra <j.pourra.perso@gmail.com>
2024-07-03 11:49:29 +0200

    cleanup

--- ts/View.ts
+++ ts/View.ts
@@ -1,6 +1,20 @@
 import { Canvas } from "./Canvas.js";
 
-export class ViewPoint {
+export type ViewProperties = {
+    x: number;
+    y: number;
+    width: number;
+    height: number;
+    halfWidth: number;
+    halfHeight: number;
+    top: number;
+    left: number;
+    bottom: number;
+    right: number;
+    element: HTMLElement;
+};
+
+export class View {
 
     private x: number;
     private y: number;
@@ -19,7 +33,7 @@ export class ViewPoint {
 
         let sizesElement = element.getBoundingClientRect();
 
-        this.updateDot(x, y);
+        this.updateCenter(x, y);
         this.updateSizes(sizesElement.width, sizesElement.height);
         this.updateArea();
 
@@ -28,20 +42,36 @@ export class ViewPoint {
 
         requestAnimationFrame(() => {
             this.canvas.clear();
-            this.canvas.drawGrid(this.x, this.y, 25);
+            this.makeGrid(25);
         });
 
         console.log("Initialize", this);
         
     }
 
+    public getProperties(): ViewProperties {
+        return {
+            x: this.x,
+            y: this.y,
+            width: this.width,
+            height: this.height,
+            halfWidth: this.halfWidth,
+            halfHeight: this.halfHeight,
+            top: this.top,
+            left: this.left,
+            bottom: this.bottom,
+            right: this.right,
+            element: this.element
+        };
+    }
+
     public onMove(shiftX: number, shiftY: number): void {
-        this.updateDot(this.x + shiftX, this.y + shiftY);
+        this.updateCenter(this.x + shiftX, this.y + shiftY);
         this.updateArea();
 
         requestAnimationFrame(() => {
             this.canvas.clear();
-            this.canvas.drawGrid(this.x, this.y, 25);
+            this.makeGrid(25);
         });
 
         console.log("Move:", this);
@@ -54,18 +84,18 @@ export class ViewPoint {
         this.updateSizes(sizesElement.width, sizesElement.height);
         this.updateArea();
 
-        this.canvas.resize(this.width, this.height);
+        this.canvas.updateSizes(this.width, this.height);
 
         requestAnimationFrame(() => {
             this.canvas.clear();
-            this.canvas.drawGrid(this.x, this.y, 25);
+            this.makeGrid(25);
         });
 
         console.log("Resize", this);
 
     }
 
-    private updateDot(x: number, y: number): void {
+    private updateCenter(x: number, y: number): void {
         this.x = x;
         this.y = y;
     }
@@ -82,6 +112,32 @@ export class ViewPoint {
         this.height = height;
         this.halfWidth = this.width / 2;
         this.halfHeight = this.height / 2;
+    }
+
+    private makeGrid(cellSize: number): void {
+
+        const halfCellSize = cellSize / 2;
+        const centerX = (this.width / 2) + this.x;
+        const centerY = (this.height / 2) + this.y;
+
+        for (let x = centerX + halfCellSize; x < this.width; x += cellSize) {
+            this.canvas.drawLine(x, 0, x, this.height);
+            this.canvas.drawTextCenter(Math.round((x - centerX) / cellSize).toString(), x + halfCellSize, centerY);
+        }
+        for (let x = centerX - halfCellSize; x > 0; x -= cellSize) {
+            this.canvas.drawLine(x, 0, x, this.height);
+            this.canvas.drawTextCenter(Math.round((x - centerX) / cellSize).toString(), x + halfCellSize, centerY);
+        }
+
+        for (let y = centerY + halfCellSize; y < this.height; y += cellSize) {
+            this.canvas.drawLine(0, y, this.width, y);
+            this.canvas.drawTextCenter(Math.round((y - centerY) / cellSize).toString(), centerX, y + halfCellSize);
+        }
+        for (let y = centerY - halfCellSize; y > 0; y -= cellSize) {
+            this.canvas.drawLine(0, y, this.width, y);
+            this.canvas.drawTextCenter(Math.round((y - centerY) / cellSize).toString(), centerX, y + halfCellSize);
+        }
+
     }
 
 }
